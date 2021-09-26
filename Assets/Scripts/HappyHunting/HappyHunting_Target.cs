9e8e7d3ef6ed0ac425f7dd88121c009d7a50f4e6
tnoell <tnoell@ucsc.edu>
2021-09-25 17:33:45 -0700

    scripts

---
+++ Assets/Scripts/HappyHunting/HappyHunting_Target.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HappyHunting_Target : MonoBehaviour
+{
+    public GameObject _crosshair;
+    public SpriteRenderer _sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _crosshair = GameObject.Find("HappyHunting_Crosshair");
+        _sprite = GetComponent<SpriteRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject citizen in _crosshair.GetComponent<HappyHunting_Crosshair>()._citizens)
+        {
+
+        }
+        
+    }
+}
